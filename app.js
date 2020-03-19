c22dc46e0191824291ad2aee0b0e139f99754782
cgraves09 <christopher.graves09@gmail.com>
2020-03-19 17:42:42 -0400

    inserted firebase and storing selection to the database. Then retrieving 3 choices from database and displaying to the results div

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+var counter = 0;
 
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCF-udTyqxcouGmz7SBrpB7Jr2BhdThzPg",
+    authDomain: "chickentender-e2f0a.firebaseapp.com",
+    databaseURL: "https://chickentender-e2f0a.firebaseio.com",
+    projectId: "chickentender-e2f0a",
+    storageBucket: "chickentender-e2f0a.appspot.com",
+    messagingSenderId: "191417021703",
+    appId: "1:191417021703:web:36a00686478c272845c1a2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
 
 $('#button').click(function(event){
     event.preventDefault();
@@ -22,17 +37,64 @@ $('#button').click(function(event){
                 // Itirate through the JSON array of 'businesses' which was returned by the API
                 for (var i = 0; i < data.businesses.length; i++){
                     var item = data.businesses
-                    var image = $('<img src="' + item[i].image_url + '"height="200" width="200">');
-                    var name = $('<h1>' + item[i].name + '</h1>');
-                    var rating = $('<h3> Rating: ' + item[i].rating + '</h3>');
-                    var category = $('<h4>' + item[i].categories[0].title + '</h4>');
-                    var divCol = $('<div class="col-md-4">')
-                    divCol.append(name,category,rating,image)
-                    
+                    var image = $('<img id="image-api" src="' + item[i].image_url + '"height="200" width="200">');
+                    var name = $('<h1 id="name-text">' + item[i].name + '</h1>');
+                    var rating = $('<h3 id="rating-text"> Rating: ' + item[i].rating + '</h3>');
+                    var category = $('<h4 id="category-text">' + item[i].categories[0].title + '</h4>');
+                    var divCol = $('<div class="col-md-4 choice">')
+                    var selectBtn = $('<br><button id="select-btn" class="btn"> Add To selection </button>')
+                    // Attaching tags to the column
+                    divCol.attr('name',item[i].name);
+                    divCol.attr('category',item[i].categories[0].title);
+                    divCol.attr('rating',item[i].rating);
+                    divCol.attr('image',item[i].image_url);
+                    divCol.append(name,category,rating,image,selectBtn);
                     divRow.append(divCol);
                     // Append our result into our page
                     $('#results').append(divRow);
             } 
+            
+            $('.choice').click(function(event){
+                event.preventDefault();
+                counter++ 
+                database.ref().push({
+                    name: $(this).attr('name'),
+                    category: $(this).attr('category'),
+                    rating: $(this).attr('rating'),
+                    image: $(this).attr('image'),
+                })
+                $(this).empty();
+                if (counter === 3) {
+                    $('#results').empty();
+                    counter = 0;
+                    retrieve();
+                }
+            })
         }
     });
 })
+
+function retrieve (){
+    var snapRow = $('<div class="row">');
+    database.ref().on('child_added', function(snapshot){
+        var snapCol = $('<div class="col-md-4 choice">');
+        var snapName = $('<h1 id="name-text">' + snapshot.val().name + '</h1>');
+        var snapCategory = $('<h4 id="category-text">' + snapshot.val().category + '</h4>');
+        var snapRating = $('<h3 id="rating-text"> Rating: ' + snapshot.val().rating + '</h3>');
+        var snapImage = $('<img id="image-api" src="' + snapshot.val().image + '"height="200" width="200">');
+        snapCol.append(snapName,snapCategory,snapRating,snapImage);
+        snapRow.append(snapCol);
+        $('#results').append(snapRow);
+    })
+    $('.choice').click(function(event){
+        event.preventDefault();
+        counter++;
+        console.log(counter);
+        if (counter === 1){
+            console.log($(this).attr('name'));
+        }
+    })
+
+}
+
+
