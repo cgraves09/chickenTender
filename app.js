aea827cf34689d24c1e78d38d0dc82921825d3fc
cgraves09 <christopher.graves09@gmail.com>
2020-03-19 15:14:53 -0400

    created variable for rating and updated <h> tags for text

--- app.js
+++ app.js
@@ -15,34 +15,27 @@ $('#button').click(function(event){
         dataType: 'json',
         success: function(data){
             $('#results').empty();
-            console.log(data.total)
-            // Grab the results from the API JSON return
-            var totalresults = data.total;
+            console.log(data.businesses)
+            
             // If our results are greater than 0, continue
-            if (totalresults > 0){
-                // Display a header on the page with the number of results
-                $('#results').append('<h5>We discovered ' + totalresults + ' results!</h5>');
+            var divRow = $('<div class="row">')
                 // Itirate through the JSON array of 'businesses' which was returned by the API
-                $.each(data.businesses, function(i, item) {
-                    // Store each business's object in a variable
-                    var id = item.id;
-                    var alias = item.alias;
-                    var phone = item.display_phone;
-                    var image = item.image_url;
-                    var name = item.name;
-                    var rating = item.rating;
-                    var reviewcount = item.review_count;
-                    var address = item.location.address1;
-                    var city = item.location.city;
-                    var state = item.location.state;
-                    var zipcode = item.location.zip_code;
+                for (var i = 0; i < data.businesses.length; i++){
+                    var item = data.businesses
+                    var image = $('<img src="' + item[i].image_url + '"height="200" width="200">');
+                    var name = $('<h1>' + item[i].name + '</h1>');
+                    var rating = $('<h3> Rating: ' + item[i].rating + '</h3>');
+                    var category = $('<h4>' + item[i].categories[0].title + '</h4>');
+                    // var city = item.location.city;
+                    // var state = item.location.state;
+                    // var zipcode = item.location.zip_code;
+                    var divCol = $('<div class="col-md-4">')
+                    divCol.append(name,category,rating,image)
+                    
+                    divRow.append(divCol);
                     // Append our result into our page
-                    $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
-                });
-            } else {
-                // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
-                $('#results').append('<h5>We discovered no results!</h5>');
-            }
+                    $('#results').append(divRow);
+            } 
         }
     });
 })
